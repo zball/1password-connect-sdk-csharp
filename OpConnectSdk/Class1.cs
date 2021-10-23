6cac6debb685cab58c3b1eeab4b4b8e444452a7d
Zac Ball <zacharywball@gmail.com>
2021-10-23 12:52:13 -0400

    initial commit

---
+++ OpConnectSdk/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OpConnectSdk
+{
+    public static class Class1
+    {
+        public static bool StartsWithUpper(this string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return false;
+
+            char ch = str[0];
+            return char.IsUpper(ch);
+        }
+    }
+}
---
+++ OpConnectSdkTest/UnitTest1.cs
@@ -0,0 +1,20 @@
+using Xunit;
+using OpConnectSdk;
+
+namespace OpConnectSdkTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestStartsWithUpper()
+        {
+            // Tests that we expect to return true.
+            string[] words = { "Alphabet", "Zebra", "ABC", "Αθήνα", "Москва" };
+            foreach (var word in words)
+            {
+                bool result = word.StartsWithUpper();
+                Assert.True(result);
+            }
+        }
+    }
+}
