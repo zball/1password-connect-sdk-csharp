d62ab854d06329ff24f2ebe249c8943a6f96de38
Mat Jones <mat@mjones.network>
2021-11-06 17:01:54 -0400

    Add FilterBuilder to help with building SCIM filters

---
+++ OpConnectSdk/Lib/Filter/FilterBuilder.cs
@@ -0,0 +1,185 @@
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+
+namespace OpConnectSdk.Lib.Filter
+{
+    public class FilterBuilder<T> {
+        private List<string> _tokens;
+
+        public FilterBuilder()
+        {
+            _tokens = new List<string>();
+        }
+
+        public FilterBuilder<T> Field<V>(Expression<Func<T, V>> fieldSelector)
+        {
+            Type type = typeof(T);
+
+            MemberExpression member = fieldSelector.Body as MemberExpression;
+            if (member == null)
+            {
+                throw new ArgumentException(string.Format("Expression '{0}' refers to a method, not a property.", fieldSelector.ToString()));
+            }
+
+            PropertyInfo propInfo = member.Member as PropertyInfo;
+            if (propInfo == null)
+            {
+                throw new ArgumentException(string.Format("Expression '{0}' refers to a field, not a property.", fieldSelector.ToString()));
+            }
+
+            if (type != propInfo.ReflectedType && !type.IsSubclassOf(propInfo.ReflectedType))
+            {
+                throw new ArgumentException(string.Format("Expression '{0}' refers to a property that is not from type {1}.", fieldSelector.ToString(), type));
+            }
+
+            var csharpFieldName = propInfo.Name;
+            if (csharpFieldName == null || csharpFieldName.Length == 0)
+            {
+                throw new ArgumentException(string.Format("Failed to retrieve property name from lambda: '{0}'", fieldSelector.ToString()));
+            }
+
+            // make the first character lowercase for the SCIM filter
+            var scimFieldName = csharpFieldName.Length > 1 ? char.ToLower(csharpFieldName[0]) + csharpFieldName.Substring(1) : csharpFieldName.ToLower();
+            _tokens.Add(scimFieldName);
+
+            return this;
+        }
+
+        public FilterBuilder<T> Group()
+        {
+            _tokens.Add("(");
+            return this;
+        }
+
+        public FilterBuilder<T> GroupEnd()
+        {
+            _tokens.Add(")");
+            return this;
+        }
+
+        public FilterBuilder<T> And()
+        {
+            _tokens.Add(ScimConstants.AND);
+            return this;
+        }
+
+        public FilterBuilder<T> Or()
+        {
+            _tokens.Add(ScimConstants.OR);
+            return this;
+        }
+
+        public FilterBuilder<T> Eq(string value)
+        {
+            _tokens.Add(ScimConstants.EQUALS);
+            _tokens.Add(value);
+            return this;
+        }
+
+        public FilterBuilder<T> Eq(long value)
+        {
+            _tokens.Add(ScimConstants.EQUALS);
+            _tokens.Add(value.ToString());
+            return this;
+        }
+
+        public FilterBuilder<T> Contains(string value)
+        {
+            _tokens.Add(ScimConstants.CONTAINS);
+            _tokens.Add(value);
+            return this;
+        }
+
+        public FilterBuilder<T> StartsWith(string value)
+        {
+            _tokens.Add(ScimConstants.STARTS_WITH);
+            _tokens.Add(value);
+            return this;
+        }
+
+        public FilterBuilder<T> IsPresent()
+        {
+            _tokens.Add(ScimConstants.PRESENT);
+            return this;
+        }
+
+        public FilterBuilder<T> GreaterThan(string value)
+        {
+            _tokens.Add(ScimConstants.GREATER_THAN);
+            _tokens.Add(value);
+            return this;
+        }
+
+        public FilterBuilder<T> GreaterThan(long value)
+        {
+            _tokens.Add(ScimConstants.GREATER_THAN);
+            _tokens.Add(value.ToString());
+            return this;
+        }
+
+        public FilterBuilder<T> GreaterThanOrEqual(string value)
+        {
+            _tokens.Add(ScimConstants.GREATER_THAN_OR_EQUAL);
+            _tokens.Add(value);
+            return this;
+        }
+
+        public FilterBuilder<T> GreaterThanOrEqual(long value)
+        {
+            _tokens.Add(ScimConstants.GREATER_THAN_OR_EQUAL);
+            _tokens.Add(value.ToString());
+            return this;
+        }
+
+        public FilterBuilder<T> LessThan(string value)
+        {
+            _tokens.Add(ScimConstants.LESS_THAN);
+            _tokens.Add(value);
+            return this;
+        }
+
+        public FilterBuilder<T> LessThan(long value)
+        {
+            _tokens.Add(ScimConstants.LESS_THAN);
+            _tokens.Add(value.ToString());
+            return this;
+        }
+
+        public FilterBuilder<T> LessThanOrEqual(string value)
+        {
+            _tokens.Add(ScimConstants.LESS_THAN_OR_EQUAL);
+            _tokens.Add(value);
+            return this;
+        }
+
+        public FilterBuilder<T> LessThanOrEqual(long value)
+        {
+            _tokens.Add(ScimConstants.LESS_THAN_OR_EQUAL);
+            _tokens.Add(value.ToString());
+            return this;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            for (var i = 0; i < _tokens.Count; i++)
+            {
+                var prevToken = i > 0 ? _tokens[i - 1] : null;
+                var token = _tokens[i];
+
+                // Don't add a space between grouping symbols and their contents
+                if (prevToken != "(" && token != ")")
+                {
+                    sb.Append(" ");
+                }
+
+                sb.Append(token);
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ OpConnectSdkTest/Lib/Filter/FilterBuilderTest.cs
@@ -0,0 +1,193 @@
+using System;
+using OpConnectSdk.Lib.Filter;
+using OpConnectSdk.Model;
+using Xunit;
+
+namespace OpConnectSdkTest
+{
+    public class FilterBuilderTest
+    {
+        # region Private Members
+
+        private FilterBuilder<Item> _sut;
+
+        # endregion Private Members
+
+        public FilterBuilderTest()
+        {
+            _sut = new FilterBuilder<Item>();
+        }
+
+        [Fact]
+        public void FilterBuilder_Field_Gets_Field_Name_From_Lambda_With_Lowercased_First_Letter()
+        {
+            // Act
+            _sut.Field(item => item.CreatedAt);
+
+            // Assert
+            Assert.Equal("createdAt", _sut.ToString());
+        }
+
+        [Fact]
+        public void FilterBuilder_Throws_Error_When_Lambda_Doesnt_Return_A_Property()
+        {
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => _sut.Field(item => 5));
+        }
+
+        [Fact]
+        public void FilterBuilder_Group_Adds_Open_Paren() {
+            // Act
+            _sut.Group();
+
+            // Assert
+            Assert.Equal("(", _sut.ToString());
+        }
+
+        [Fact]
+        public void FilterBuilder_GroupEnd_Adds_Close_Paren() {
+            // Act
+            _sut.GroupEnd();
+
+            // Assert
+            Assert.Equal(")", _sut.ToString());
+        }
+
+        [Fact]
+        public void FilterBuilder_And_Adds_Scim_And()
+        {
+            // Act
+            _sut.And();
+
+            // Assert
+            Assert.Equal(ScimConstants.AND, _sut.ToString());
+        }
+
+        [Fact]
+        public void FilterBuilder_Or_Adds_Scim_Or()
+        {
+            // Act
+            _sut.Or();
+
+            // Assert
+            Assert.Equal(ScimConstants.OR, _sut.ToString());
+        }
+
+        [Fact]
+        public void FilterBuilder_Eq_String_Adds_Scim_Eq_Value()
+        {
+            // Act
+            _sut.Eq("test");
+
+            // Assert
+            Assert.Equal($"{ScimConstants.EQUALS} test", _sut.ToString());
+        }
+
+        [Fact]
+        public void FilterBuilder_Eq_Long_Adds_Scim_Eq_Value_ToString()
+        {
+            // Act
+            _sut.Eq(5);
+
+            // Assert
+            Assert.Equal($"{ScimConstants.EQUALS} 5", _sut.ToString());
+        }
+
+        [Fact]
+        public void FilterBuilder_Contains_Adds_Scim_Co_Value()
+        {
+            // Act
+            _sut.Contains("test");
+
+            // Assert
+            Assert.Equal($"{ScimConstants.CONTAINS} test", _sut.ToString());
+        }
+
+        [Fact]
+        public void FilterBuilder_StartsWith_Adds_Scim_Sw()
+        {
+            // Act
+            _sut.StartsWith("test");
+
+            // Assert
+            Assert.Equal($"{ScimConstants.STARTS_WITH} test", _sut.ToString());
+        }
+
+        [Fact]
+        public void FilterBuilder_IsPresent_Adds_Scim_Pr()
+        {
+            // Act
+            _sut.Field(item => item.Title).IsPresent();
+
+            // Assert
+            Assert.Equal($"title {ScimConstants.PRESENT}", _sut.ToString());
+        }
+
+        [Fact]
+        public void FilterBuilder_GreaterThan_Adds_Scim_Gt()
+        {
+            // Act
+            _sut.GreaterThan(5);
+
+            // Assert
+            Assert.Equal($"{ScimConstants.GREATER_THAN} 5", _sut.ToString());
+        }
+
+        [Fact]
+        public void FilterBuilder_GreaterThanOrEqual_Adds_Scim_Ge()
+        {
+            // Act
+            _sut.GreaterThanOrEqual(5);
+
+            // Assert
+            Assert.Equal($"{ScimConstants.GREATER_THAN_OR_EQUAL} 5", _sut.ToString());
+        }
+
+        [Fact]
+        public void FilterBuilder_LessThan_Adds_Scim_Lt()
+        {
+            // Act
+            _sut.LessThan(5);
+
+            // Assert
+            Assert.Equal($"{ScimConstants.LESS_THAN} 5", _sut.ToString());
+        }
+
+        [Fact]
+        public void FilterBuilder_LessThanOrEqual_Adds_Scim_Le()
+        {
+            // Act
+            _sut.LessThanOrEqual(5);
+
+            // Assert
+            Assert.Equal($"{ScimConstants.LESS_THAN_OR_EQUAL} 5", _sut.ToString());
+        }
+
+        [Fact]
+        public void FilterBuilder_ToString_Does_Not_Add_Space_Between_Parenthesis_And_Inner_Terms()
+        {
+            // Arrange
+            var expectedFilter = "(title eq test or title eq test2) and (id eq test3 or id eq test4)";
+
+            // Act
+            _sut.Group()
+                .Field(item => item.Title)
+                .Eq("test")
+                .Or()
+                .Field(item => item.Title)
+                .Eq("test2")
+                .GroupEnd()
+                .And()
+                .Group()
+                .Field(item => item.Id)
+                .Eq("test3")
+                .Or()
+                .Field(item => item.Id)
+                .Eq("test4")
+                .GroupEnd();
+
+            // Assert
+            Assert.Equal(expectedFilter, _sut.ToString());
+        }
+    }
+}
